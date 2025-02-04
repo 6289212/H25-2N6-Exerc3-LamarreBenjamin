c7a1e1e1d8959d1c435caab1cdc3fd2717a589b7
6289212 <6289212@cegepmontpetit.ca>
2025-02-04 15:47:17 -0500

    Ajout R03

---
+++ R03 Exercices Depart/Ex2 OS.py
@@ -0,0 +1,54 @@
+import os
+os.chdir(os.path.dirname(__file__)) # cette ligne change le répertoire courant pour qu'il devienne celui où ce trouve le fichier actuel.
+
+# Q1  Imprimez le répertoire courant
+print(f"Q1{'_'*60}")
+print(os.getcwd())
+
+
+# Q2   Imprimez la variable d'environnement USERPROFILE (utilisez la méthode .get() de l'objet os.environ)
+print(f"Q2{'_'*60}")
+print(os.environ.get("USERPROFILE"))
+
+
+# Q3 Déplacez-vous sur le répertoire 'Documents' et imprimez le répertoire courant
+#       Attention : n'utilisez pas un chemin relatif.
+print(f"Q3{'_'*60}")
+os.chdir("C:\\Users\\6289212\\Documents")
+print(os.getcwd())
+
+# Q4   Imprimez la liste des répertoires et des fichiers qu'il y a dans 'Document'
+print(f"Q4{'_'*60}")
+print(os.listdir())
+
+
+# Q5   Créez un répertoire OS-ExercQ5
+#       Réimprimez les répertoires et les fichiers dans 'Document'
+print(f"Q5{'_'*60}")
+if "OS-ExercQ5" in os.listdir() :
+    pass
+else :
+    os.mkdir("OS-ExercQ5")
+print(os.listdir())
+
+
+# Q6   Créez les répertoires OS-ExercQ6/Subdir1 avec une seule instruction
+#       Réimprimez les répertoires et les fichiers dans votre 'Document'
+print(f"Q6{'_'*60}")
+
+print(os.listdir("C:\\Users\\6289212\\Documents\\OS-ExercQ5"))
+
+
+#Q7   Renommez le répertoire Subdir1 pour qu'il s'appelle Sous_repertoire
+print(f"Q6{'_'*60}")
+
+
+
+# Q8   suppression du répertoire OS-ExercQ6 et de son contenu
+#       Réimprimez les répertoires et les fichiers dans votre 'Document'
+print(f"Q6{'_'*60}")
+
+
+
+
+
